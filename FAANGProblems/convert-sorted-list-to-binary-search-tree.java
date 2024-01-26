6b70bb9eed517773ef567aa320d2bf49b37d016d
Priya Gupta <37833111+Priygupt13@users.noreply.github.com>
2024-01-26 14:03:52 -0800

    Create convert-sorted-list-to-binary-search-tree.java

---
+++ FAANGProblems/convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,24 @@
+
+https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+Tc : O(n(log(n)
+Sc : O(log(n)  
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+       if(head==null) return null;
+       if(head.next==null) return new TreeNode(head.val);
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while(fast!=null && fast.next!=null){
+             prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        prev.next = null;
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+    
+}
