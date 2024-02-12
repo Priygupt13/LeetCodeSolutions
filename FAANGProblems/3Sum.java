1d01a43d1a1fe7c0e49d2c6301a58673cac486b0
Priya Gupta <37833111+Priygupt13@users.noreply.github.com>
2024-02-12 12:43:44 -0800

    Create 3Sum.java

---
+++ FAANGProblems/3Sum.java
@@ -0,0 +1,42 @@
+import java.util.*;
+TC : O(nlogn) + O(n^2) = O(n^2)
+SC : O(1)  
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int left = i + 1, right = nums.length - 1, target = -nums[i];
+                while (left < right) {
+                    if (nums[left] + nums[right] == target) {
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ThreeSum solution = new ThreeSum();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = solution.threeSum(nums);
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+}
